41c0b76c30e1710d556881bb479997be7a9308fe
Preethi0502 <131513045+Preethi0502@users.noreply.github.com>
2024-12-14 11:25:47 +0530

    Update ResumePage.js

--- src/pages/resume_page/ResumePage.js
+++ src/pages/resume_page/ResumePage.js
@@ -2,22 +2,26 @@ import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import Particle from "../../Particle";
-import pdf from "../../assets/MD_MAFUJUL_HASAN.pdf";
+import pdf from "../../assets/FreightFox P1.pdf";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import Zoom from "react-reveal/Zoom";
 import React, { useState, useEffect } from "react";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://raw.githubusercontent.com/MD-MAFUJUL-HASAN/Personal-Portfolio/main/src/assets/MD_MAFUJUL_HASAN.pdf";
+const resumeLink = "https://drive.google.com/uc?export=download&id=1MCEAXWli5AZ9wKWQ8vE-UQXPifspES";
 
 function Resume() {
   const [width, setWidth] = useState(1200);
+  const [numPages, setNumPages] = useState(null);
 
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
   return (
     <div>
@@ -36,18 +40,23 @@ function Resume() {
             </div>
             <div fluid className="certificate-section" id="about">
               <div className="d-flex justify-content-center mt-4">
-                <Button variant="primary" href={pdf} target="_blank">
+                <Button variant="primary" href={resumeLink} target="_blank">
                   <AiOutlineDownload />
                   &nbsp;Download Resume
                 </Button>
               </div>
               <div className="resume d-flex justify-content-center">
-                <Document file={resumeLink}>
-                  <Page pageNumber={1} scale={width > 786 ? 1.6 : 0.4} />
+                <Document
+                  file={pdf}
+                  onLoadSuccess={onDocumentLoadSuccess}
+                >
+                  {Array.from(new Array(numPages), (el, index) => (
+                    <Page key={`page_${index + 1}`} pageNumber={index + 1} scale={width > 786 ? 1.6 : 0.4} />
+                  ))}
                 </Document>
               </div>
               <div className="d-flex justify-content-center">
-                <Button variant="primary" href={pdf} target="_blank">
+                <Button variant="primary" href={resumeLink} target="_blank">
                   <AiOutlineDownload />
                   &nbsp;Download Resume
                 </Button>
