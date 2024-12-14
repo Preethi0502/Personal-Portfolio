609eece95b0548d01cf768c8f1d5b5ea4c3f2d1e
Preethi0502 <131513045+Preethi0502@users.noreply.github.com>
2024-12-14 10:03:41 +0530

    Update Projects.js

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,287 +1,43 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import Zoom from "react-reveal/Zoom";
-import { Fade } from "react-reveal";
-import nyc from "../../images/nyc.png";
-import ci from "../../images/onboarding.png";
-import hd from "../../images/hd.png";
-import pp from "../../images/portfolio.png";
-import { FaCode } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { HiArrowRight } from "react-icons/hi";
+import project1 from "../../images/1.jpeg";
+import project2 from "../../images/2.jpeg";
+import project3 from "../../images/3.jpeg";
+import project4 from "../../images/4.jpeg";
+import project5 from "../../images/5.png";
 
-export default function Projects() {
-  return (
-    <div>
-      <Container fluid className="certificate-section" id="about">
-        <Container>
-          <Row>
-            <Col
-              md={12}
-              className="certificate-description d-flex justify-content-start"
-            >
-              <Zoom left cascade>
-                <h1 className="aboutme-heading">Projects</h1>
-              </Zoom>
-            </Col>
-            <Col md={3}>
-              <Fade bottom>
-                <div
-                  key={1}
-                  className="singleProject"
-                  style={{
-                    backgroundColor: "rgb(142 70 186 / 31%)",
-                    border: "1px solid",
-                  }}
-                >
-                  <div className="projectContent">
-                    <h5 id={"first"} style={{ color: "#fbd9ad" }}>
-                      New Year CountDown
-                    </h5>
-                    <img src={nyc} alt={nyc} />
-                    <div className="project--showcaseBtn">
-                      <a
-                        href={
-                          "https://github.com/MD-MAFUJUL-HASAN/Web_Programming/tree/main/New-Year-Countdown"
-                        }
-                        target="_blank"
-                        rel="noreferrer"
-                        className={"iconBtn"}
-                        aria-labelledby={`code`}
-                      >
-                        <FaCode
-                          id={`code`}
-                          className={"icon"}
-                          aria-label="Code"
-                        />
-                      </a>
-                    </div>
-                  </div>
-                  <h6>
-                    <p
-                      className="project--desc"
-                      style={{
-                        background: "#fbd9ad",
-                        color: "#b061df",
-                        fontWeight: 600,
-                      }}
-                    >
-                      The New Year Countdown Project is more than just a
-                      celebration; it's a testament to the human spirit and our
-                      shared aspirations for a brighter future. So, mark your
-                      calendars and get ready to be part of this global
-                      extravaganza as we count down to a new year filled with
-                      endless possibilities.
-                    </p>
-                  </h6>
-                  <div
-                    className="project--lang"
-                    style={{
-                      background: "#fbd9ad",
-                      color: "#b061df",
-                      fontWeight: 600,
-                    }}
-                  >
-                    HTML, CSS, JavaScript
-                  </div>
-                </div>
-              </Fade>
-            </Col>
-            <Col md={3}>
-              <Fade bottom>
-                <div
-                  key={1}
-                  className="singleProject"
-                  style={{
-                    backgroundColor: "rgb(142 70 186 / 31%)",
-                    border: "1px solid",
-                  }}
-                >
-                  <div className="projectContent">
-                    <h5 id={"first"} style={{ color: "#fbd9ad" }}>
-                      Heart Disease Prediction
-                    </h5>
-                    <img src={hd} alt={hd} />
-                    <div className="project--showcaseBtn">
-                      <a
-                        href={
-                          "https://github.com/MD-MAFUJUL-HASAN/Machine_Learning/tree/main/Heart%20Disease%20Prediction"
-                        }
-                        target="_blank"
-                        rel="noreferrer"
-                        className={"iconBtn"}
-                        aria-labelledby={`code`}
-                      >
-                        <FaCode
-                          id={`code`}
-                          className={"icon"}
-                          aria-label="Code"
-                        />
-                      </a>
-                    </div>
-                  </div>
-                  <h6>
-                    <p
-                      className="project--desc"
-                      style={{
-                        background: "#fbd9ad",
-                        color: "#b061df",
-                        fontWeight: 600,
-                      }}
-                    >
-                      Use of AI and Machine Learning: Artificial intelligence
-                      and machine learning are already being used in healthcare
-                      to help predict heart disease. In the future, these
-                      technologies could become more advanced, enabling more
-                      accurate predictions of heart disease risk.
-                    </p>
-                  </h6>
-                  <div
-                    className="project--lang"
-                    style={{
-                      background: "#fbd9ad",
-                      color: "#b061df",
-                      fontWeight: 600,
-                    }}
-                  >
-                    Pandas, Matplotlib, Scikit-Learn
-                  </div>
-                </div>
-              </Fade>
-            </Col>
-            <Col md={3}>
-              <Fade bottom>
-                <div
-                  key={1}
-                  className="singleProject"
-                  style={{
-                    backgroundColor: "rgb(142 70 186 / 31%)",
-                    border: "1px solid",
-                  }}
-                >
-                  <div className="projectContent">
-                    <h5 id={"first"} style={{ color: "#fbd9ad" }}>
-                      Online Payroll Management System
-                    </h5>
-                    <img src={ci} alt={ci} />
-                    <div className="project--showcaseBtn">
-                      <a
-                        href={
-                          "https://github.com/MD-MAFUJUL-HASAN/Online-Payroll-Management-System"
-                        }
-                        target="_blank"
-                        rel="noreferrer"
-                        className={"iconBtn"}
-                        aria-labelledby={`code`}
-                      >
-                        <FaCode
-                          id={`code`}
-                          className={"icon"}
-                          aria-label="Code"
-                        />
-                      </a>
-                    </div>
-                  </div>
-                  <h6>
-                    <p
-                      className="project--desc"
-                      style={{
-                        background: "#fbd9ad",
-                        color: "#b061df",
-                        fontWeight: 600,
-                      }}
-                    >
-                      A Payroll management System with feature like generate
-                      payslip & reports. It manages salaries, leaves, branches,
-                      employees, designations, shifts, holidays and employee
-                      attendance. It has chat application so internal employees
-                      can use that feature.
-                    </p>
-                  </h6>
-                  <div
-                    className="project--lang"
-                    style={{
-                      background: "#fbd9ad",
-                      color: "#b061df",
-                      fontWeight: 600,
-                    }}
-                  >
-                    CSS, JavaScript, PHP, SCSS
-                  </div>
-                </div>
-              </Fade>
-            </Col>
-            <Col md={3}>
-              <Fade bottom>
-                <div
-                  key={1}
-                  className="singleProject"
-                  style={{
-                    backgroundColor: "rgb(142 70 186 / 31%)",
-                    border: "1px solid",
-                  }}
-                >
-                  <div className="projectContent">
-                    <h5 id={"first"} style={{ color: "#fbd9ad" }}>
-                      Personal Portfolio
-                    </h5>
-                    <img src={pp} alt={pp} />
-                    <div className="project--showcaseBtn">
-                      <a
-                        href={
-                          "https://github.com/MD-MAFUJUL-HASAN/Personal-Portfolio"
-                        }
-                        target="_blank"
-                        rel="noreferrer"
-                        className={"iconBtn"}
-                        aria-labelledby={`code`}
-                      >
-                        <FaCode
-                          id={`code`}
-                          className={"icon"}
-                          aria-label="Code"
-                        />
-                      </a>
-                    </div>
-                  </div>
-                  <h6>
-                    <p
-                      className="project--desc"
-                      style={{
-                        background: "#fbd9ad",
-                        color: "#b061df",
-                        fontWeight: 600,
-                      }}
-                    >
-                      Welcome to my personal portfolio. Explore my work, skills,
-                      and achievements in a concise and visually appealing
-                      format. Get a glimpse of my expertise and creativity.
-                    </p>
-                  </h6>
-                  <div
-                    className="project--lang"
-                    style={{
-                      background: "#fbd9ad",
-                      color: "#b061df",
-                      fontWeight: 600,
-                    }}
-                  >
-                    HTML, CSS, SCSS, React, JavaScript
-                  </div>
-                </div>
-              </Fade>
-            </Col>
-          </Row>
-          <div className="blog--viewAll">
-            <Link to="/projectspage">
-              <button className="btn btn-primary">
-                View All
-                <HiArrowRight className="viewArr" />
-              </button>
-            </Link>
-          </div>
-        </Container>
-      </Container>
-    </div>
-  );
-}
+export const projects = [
+  {
+    title: 'Enhanced Blockchain-Based Decentralized Public Auditing',
+    image: project1,
+    githubLink: 'https://github.com/Preethi0502/Enhanced-Blockchain-Based-Decentralized-Public-Auditing-for-Cloud-Storage',
+    description: 'Developed a privacy-preserving protocol for secure access and data sharing in cloud storage.',
+    technologies: 'Blockchain, Attribute-Based Access Control, Proxy Re-Encryption'
+  },
+  {
+    title: 'Full-Stack MERN App',
+    image: project2,
+    githubLink: 'https://github.com/Preethi0502/Memoirs-Full-Stack-MERN-Application',
+    description: 'Built a fully functional MERN-based social media platform, Memoirs, for sharing life moments.',
+    technologies: 'React, Node.js, Express, MongoDB, Redux'
+  },
+  {
+    title: 'GitHub Actions Documentation',
+    image: project3,
+    githubLink: 'https://github.com/Preethi0502/github-actions-auto-markdown-docs',
+    description: 'Created a GitHub Action that auto-generates Markdown documentation for workflows directly from the action.yml file.',
+    technologies: 'GitHub Actions, YAML, Markdown, Git'
+  },
+  {
+    title: 'AI Job Recommendation System',
+    image: project4,
+    githubLink: 'https://github.com/Preethi0502/AI-Job-Recommendation-System',
+    description: 'Designed a job recommendation system leveraging ML and NLP to analyze skill similarities and suggest opportunities.',
+    technologies: 'Python, Flask, Scikit-learn, Spacy, MongoDB'
+  },
+  {
+    title: 'Custom Redis Server',
+    image: project5,
+    githubLink: 'https://github.com/Preethi0502/PyRedis-Server',
+    description: 'Engineered a Redis-like server in Python to replicate data storage and retrieval functions, including TTL-based key expiration.',
+    technologies: 'Python (3.7+), Redis Protocol, Shell Scripting, Threading, Ubuntu/Linux'
+  }
+];
