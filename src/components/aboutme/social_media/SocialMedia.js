fa5ac3f288f8d37e453e12ac3a10bfc04c5b99db
Preethi0502 <131513045+Preethi0502@users.noreply.github.com>
2024-12-14 09:57:01 +0530

    Update SocialMedia.js

--- src/components/aboutme/social_media/SocialMedia.js
+++ src/components/aboutme/social_media/SocialMedia.js
@@ -1,67 +1,52 @@
 import React from "react";
-import {
-  AiOutlineInstagram,
-  AiFillFacebook,
-  AiFillGithub,
-  AiFillLinkedin,
-} from "react-icons/ai";
+import { FaHackerrank } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import Zoom from "react-reveal/Zoom";
 import { Container, Row } from "react-bootstrap";
 import { Col } from "react-bootstrap";
-import Zoom from "react-reveal/Zoom";
 
-export default function SocialMedia() {
+export default function Ranking() {
   return (
     <div className="mt-4">
       <Zoom left cascade>
-        <h1>Connect with me</h1>
+        <h1>My Profiles</h1>
       </Zoom>
       <Container className="mt-5">
         <Row className="g-5">
-          <Col md={3}>
+          <Col md={4}>
             <a
-              href="https://www.instagram.com/_md_mafujul_hasan_/"
+              href="https://www.hackerrank.com/profile/preethi522002"
               target="_blank"
               rel="noreferrer"
               className="icon-colour  home-social-icons"
             >
               <Zoom cascade>
-                <AiOutlineInstagram />
+                <FaHackerrank />
               </Zoom>
             </a>
           </Col>
-          <Col md={3}>
+          <Col md={4}>
             <a
-              href="https://www.facebook.com/mh.tonmoy.13"
+              href="https://www.linkedin.com/in/preethi522/"
               target="_blank"
               rel="noreferrer"
               className="icon-colour  home-social-icons"
             >
               <Zoom cascade>
-                <AiFillFacebook />
+                <FaLinkedin />
               </Zoom>
             </a>
           </Col>
-          <Col md={3}>
+          <Col md={4}>
             <a
-              href="https://github.com/MD-MAFUJUL-HASAN"
+              href="https://github.com/Preethi0502"
               target="_blank"
               rel="noreferrer"
               className="icon-colour  home-social-icons"
             >
               <Zoom cascade>
-                <AiFillGithub />
-              </Zoom>
-            </a>
-          </Col>
-          <Col md={3}>
-            <a
-              href="https://www.linkedin.com/in/md-mafujul-hasan/"
-              target="_blank"
-              rel="noreferrer"
-              className="icon-colour  home-social-icons"
-            >
-              <Zoom cascade>
-                <AiFillLinkedin />
+                <FaGithub />
               </Zoom>
             </a>
           </Col>
